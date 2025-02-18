95a6bf001331a72874065a751ef0bedec2360985
SeverusBlue <julian.online@icloud.com>
2025-02-18 19:48:21 +0100

    fgdgd

--- Project P/Assets/MapToLake.cs
+++ Project P/Assets/MapToLake.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapToLake
 {
-    
+    public string lake;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(lake);
+    }
 }
