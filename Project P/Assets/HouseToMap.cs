9bc8d4b82acf5d49fef3064be9fe90d8ba8f54ae
SeverusBlue <julian.online@icloud.com>
2025-02-18 20:45:12 +0100

    Update HouseToMap.cs

--- Project P/Assets/HouseToMap.cs
+++ Project P/Assets/HouseToMap.cs
@@ -3,7 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class HouseToMap : MonoBehaviour
 {
+    public AudioSource transitionSound; // AudioSource für den Übergangssound
+
     public void LoadScene()
+    {
+        if (transitionSound != null)
+        {
+            // Sound abspielen
+            transitionSound.Play();
+
+            // Szene erst nach der Länge des Sounds wechseln
+            Invoke("ChangeScene", transitionSound.clip.length);
+        }
+        else
+        {
+            // Falls kein Sound gesetzt ist, sofort wechseln
+            ChangeScene();
+        }
+    }
+
+    void ChangeScene()
     {
         SceneManager.LoadScene("Map");
     }
