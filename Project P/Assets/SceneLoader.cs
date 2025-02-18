ef2b6d7bd766041249e10976fbf54dc5a6738fd2
bauergherkin <silas2911@icloud.com>
2025-02-18 19:25:29 +0100

    SceneLoader

--- Project P/Assets/SceneLoader.cs
+++ Project P/Assets/SceneLoader.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void LoadScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Map");
     }
 }
