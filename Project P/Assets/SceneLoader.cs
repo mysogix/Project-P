1436fe9464f6f7f73140f3e8d41443a6c455fbb8
bauergherkin <silas2911@icloud.com>
2025-02-19 21:07:53 +0100

    scheise

--- Project P/Assets/SceneLoader.cs
+++ Project P/Assets/SceneLoader.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public Animator transitionAnimator;
+    public string sceneLoad;
+
     public void LoadScene()
     {
         SceneManager.LoadScene("Scene 1");
