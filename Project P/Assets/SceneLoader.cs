ecc307ccde8bb885283ac362afb2b4a34ca3290d
mysogix <jonas@pixel-brain.de>
2025-02-25 20:57:02 +0100

    a whole lot

--- Project P/Assets/SceneLoader.cs
+++ Project P/Assets/SceneLoader.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public Animator transitionAnimator;
-    public string sceneLoad;
+    public AudioSource transitionSound;
 
-    public void LoadScene()
+    public string SceneName;
+    public Animator transition;
+    public float transitionTime = 1f;
+   public void ChangeScene()
     {
-        SceneManager.LoadScene("Scene 1");
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+        
+        transitionSound.Play();
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(SceneName);
     }
 }
